10c23250067589774b8eb32ee8a22be9d7c6204b
yanjun <yanjun@shiyanjun.cn>
2015-02-05 11:54:59 +0800

    Optimize codes.

--- storm-realtime-analytics/src/main/java/org/shirdrn/storm/analytics/common/AbstractJedisEventHandler.java
+++ storm-realtime-analytics/src/main/java/org/shirdrn/storm/analytics/common/AbstractJedisEventHandler.java
@@ -1,12 +1,15 @@
 package org.shirdrn.storm.analytics.common;
 
 import java.util.Collection;
+import java.util.NoSuchElementException;
 import java.util.TreeSet;
 
 import net.sf.json.JSONObject;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import redis.clients.jedis.Jedis;
 
 import com.google.common.collect.Sets;
 
@@ -26,10 +29,24 @@ public abstract class AbstractJedisEventHandler extends
 		LOG.info(this.getClass().getSimpleName() + ": indicators=" + mappedIndicators);
 		TreeSet<AbstractResult> results = Sets.newTreeSet();
 		for(int indicator : mappedIndicators) {
-			super.compute(results, indicator, event);
+			compute(results, indicator, event);
 		}
 		LOG.info(this.getClass().getSimpleName() + ": results=" + results);
 		return results;
+	}
+	
+	protected void compute(TreeSet<AbstractResult> results, int indicator, JSONObject event) {
+		Jedis jedis = jedisBolt.getJedis();
+		IndicatorCalculator<? extends AbstractResult> calculator = selectCalculator(indicator);
+		if(calculator == null) {
+			throw new NoSuchElementException("Not found calculator for indicator: " + indicator);
+		}
+		calculator.setPrintRedisCmdLogLevel(jedisBolt.getRedisCmdLogLevel());
+		AbstractResult result = calculator.calculate(jedis, event);
+		if(result != null) {
+			results.add(result);
+		}
+		jedisBolt.returnResource(jedis);
 	}
 	
 }
--- storm-realtime-analytics/src/main/java/org/shirdrn/storm/analytics/common/JedisEventHandler.java
+++ storm-realtime-analytics/src/main/java/org/shirdrn/storm/analytics/common/JedisEventHandler.java
@@ -1,10 +1,5 @@
 package org.shirdrn.storm.analytics.common;
 
-import java.util.NoSuchElementException;
-import java.util.TreeSet;
-
-import net.sf.json.JSONObject;
-import redis.clients.jedis.Jedis;
 
 public abstract class JedisEventHandler<R, E> extends MappedEventHandler<R, E> {
 
@@ -14,20 +9,6 @@ public abstract class JedisEventHandler<R, E> extends MappedEventHandler<R, E> {
 	public JedisEventHandler(JedisRichBolt jedisBolt, String eventCode) {
 		super(eventCode);
 		this.jedisBolt = jedisBolt;
-	}
-	
-	protected void compute(TreeSet<AbstractResult> results, int indicator, JSONObject event) {
-		Jedis jedis = jedisBolt.getJedis();
-		IndicatorCalculator<? extends AbstractResult> calculator = selectCalculator(indicator);
-		if(calculator == null) {
-			throw new NoSuchElementException("Not found calculator for indicator: " + indicator);
-		}
-		calculator.setPrintRedisCmdLogLevel(jedisBolt.getRedisCmdLogLevel());
-		AbstractResult result = calculator.calculate(jedis, event);
-		if(result != null) {
-			results.add(result);
-		}
-		jedisBolt.returnResource(jedis);
 	}
 	
 }
